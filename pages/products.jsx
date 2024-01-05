d3bd1461e8f0f629152e97ddb9616b8e7565e64b
web10crown <neeraj13221822@gmail.com>
2024-01-05 23:18:35 +0530

    axios timeout

--- pages/products.jsx
+++ pages/products.jsx
@@ -64,7 +64,7 @@ export const getServerSideProps = async ({ query }) => {
 		} else if (find) {
 			res = await axios.get(`${publicRequest}/api/products?find=${find}`);
 		} else {
-			res = await axios.get(`${publicRequest}/api/products`);
+			res = await axios.get(`${publicRequest}/api/products`,{ timeout: 10000 });
 		}
 
 		return {
