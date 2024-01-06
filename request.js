f2aba3b2777b1255024513de7fcc1095df19ff89
web10crown <neeraj13221822@gmail.com>
2024-01-06 14:49:49 +0530

    request url change

--- request.js
+++ request.js
@@ -1,4 +1,4 @@
 const publicRequest =
-    process.env.NODE_ENV === 'production' ? "https://amazon-netlify.vercel.app" : "http://localhost:3000";
+    process.env.NODE_ENV === 'production' ? "https://amazon-crown.netlify.app" : "http://localhost:3000";
 
 export { publicRequest };
