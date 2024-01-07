d6d9ceb3e32aa63d8867559dd5c9d44ab853a20e
web10crown <neeraj13221822@gmail.com>
2024-01-07 21:46:49 +0530

    correcting loader for all pages

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -6,12 +6,14 @@ import Image from "next/image";
 import axios from "axios";
 import { useDispatch } from "react-redux";
 import { add } from "@/redux/cartSlice";
+import Loader from '@/components/Loader';
 
 const Product = () => {
 	const router = useRouter();
 	const dispatch = useDispatch();
 	const [product, setProduct] = useState(null);
 	const [quantity, setQuantity] = useState(1);
+	const [loading, setLoading] = useState(true);
 
 	const addProduct = () => {
 		if (product) {
@@ -27,89 +29,86 @@ const Product = () => {
 
 				const res = await axios.get(`${publicRequest}/api/products/${id}`);
 				setProduct(res.data);
+				setLoading(false);
 			} catch (error) {
 				console.error(`Error fetching product with ID ${router.query.id}:`, error);
 			}
 		};
-
-		if (router.query.id) {
-			fetchData();
-		}
+		fetchData();
 	}, [router.query.id]);
-
-	if (!product) {
-		return <p>Loading...</p>;
-	}
+	
 
 	return (
 		<>
-			<div className={styles.container}>
-				<h5>{product.cat.toUpperCase() + " >"}</h5>
-				<div className={styles.wrapper}>
-					<div className={styles.left}>
-						<div className={styles.imgContainer}>
-							<div className={styles.img}>
-								<Image
-									src={`${product.img.replace(/^http:/, 'https:')}`}
-									alt="image"
-									fill
-									style={{ objectFit: "contain" }}
-									sizes="auto"
-									priority={true}
+			{loading ? <Loader /> : (
+				<div className={styles.container}>
+					<h5>{product.cat.toUpperCase() + " >"}</h5>
+					<div className={styles.wrapper}>
+						<div className={styles.left}>
+							<div className={styles.imgContainer}>
+								<div className={styles.img}>
+									<Image
+										src={`${product.img.replace(/^http:/, 'https:')}`}
+										alt="image"
+										fill
+										style={{ objectFit: "contain" }}
+										sizes="auto"
+										priority={true}
+									/>
+								</div>
+							</div>
+						</div>
+						<div className={styles.center}>
+							<div className={styles.heading}>
+								<h2>{product.desc}</h2>
+							</div>
+							<hr />
+							<div className={styles.price}>
+								<p style={{ color: "red" }}>Deal of the Day</p>
+								<div className={styles.amount}>
+									<h4>-26%</h4>
+									<h3>₹ {product.price}</h3>
+								</div>
+								<p>Inclusive of all taxes</p>
+								<p>
+									<b>EMI</b> starts at ₹
+									{Math.round(product.price / 6)} No Cost EMI
+									available
+								</p>
+							</div>
+							<hr />
+							<div className={styles.details}>
+								<div className={styles.dHead}>
+									<p>Brand</p>
+									<p>Name</p>
+									<p>Cat</p>
+									<p>Price</p>
+									<p>Details</p>
+								</div>
+								<div className={styles.dDetails}>
+									<p>{product.brand}</p>
+									<p>{product.name}</p>
+									<p>{product.cat}</p>
+									<p>{product.price}</p>
+									<p>{product.desc.slice(0, 50)}...</p>
+								</div>
+							</div>
+							<div className={styles.amountContainer}>
+								<input
+									type="number"
+									min="1"
+									defaultValue="1"
+									onChange={(e) => setQuantity(e.target.value)}
 								/>
+								<button onClick={addProduct}>Add +</button>
 							</div>
 						</div>
-					</div>
-					<div className={styles.center}>
-						<div className={styles.heading}>
-							<h2>{product.desc}</h2>
+						<div className={styles.right}>
+							<h4>No offers available</h4>
 						</div>
-						<hr />
-						<div className={styles.price}>
-							<p style={{ color: "red" }}>Deal of the Day</p>
-							<div className={styles.amount}>
-								<h4>-26%</h4>
-								<h3>₹ {product.price}</h3>
-							</div>
-							<p>Inclusive of all taxes</p>
-							<p>
-								<b>EMI</b> starts at ₹
-								{Math.round(product.price / 6)} No Cost EMI
-								available
-							</p>
-						</div>
-						<hr />
-						<div className={styles.details}>
-							<div className={styles.dHead}>
-								<p>Brand</p>
-								<p>Name</p>
-								<p>Cat</p>
-								<p>Price</p>
-								<p>Details</p>
-							</div>
-							<div className={styles.dDetails}>
-								<p>{product.brand}</p>
-								<p>{product.name}</p>
-								<p>{product.cat}</p>
-								<p>{product.price}</p>
-								<p>{product.desc.slice(0, 50)}...</p>
-							</div>
-						</div>
-						<div className={styles.amountContainer}>
-							<input
-								type="number"
-								min="1"
-								defaultValue="1"
-								onChange={(e) => setQuantity(e.target.value)}
-							/>
-							<button onClick={addProduct}>Add +</button>
-						</div>
-					</div>
-					<div className={styles.right}>
-						<h4>No offers available</h4>
 					</div>
 				</div>
-			</div>
+			)} 
 		</>
 	);
 };
--- pages/products.jsx
+++ pages/products.jsx
@@ -1,15 +1,16 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, use } from 'react';
 import { useRouter } from 'next/router';
 import styles from "@/styles/pages/Products.module.css";
 import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
 import { publicRequest } from "../request";
+import Loader from '@/components/Loader';
 
 export default function Products() {
   const [data, setData] = useState([]);
   const router = useRouter();
-
+  const [loading, setloading] = useState(true);
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -18,11 +19,12 @@ export default function Products() {
           cat
             ? `${publicRequest}/api/products?cat=${cat}`
             : find
-            ? `${publicRequest}/api/products?find=${find}`
-            : `${publicRequest}/api/products`;
+              ? `${publicRequest}/api/products?find=${find}`
+              : `${publicRequest}/api/products`;
 
         const res = await axios.get(endpoint);
         setData(res.data);
+        setloading(false);
       } catch (err) {
         console.error(err);
       }
@@ -32,47 +34,51 @@ export default function Products() {
   }, [router.query]);
 
   return (
-    <div className={styles.container}>
-      <h1>{data[0] ? "PRODUCTS " : "Not Available"}</h1>
-      <div className={styles.wrapper}>
-        <div className={styles.filters}>
-          <h2>Average Customer Review</h2>
-          <p>filter 1</p>
-        </div>
-        <div className={styles.cardContainer}>
-          {data.slice().reverse().map((p) => (
-            <div className={styles.card} key={p._id}>
-              <Link
-                href={`/product/${p._id}`}
-                style={{
-                  color: "#131921",
-                  textDecoration: "none",
-                }}
-              >
-                <>
-                  <div className={styles.imgContainer}>
-                    <Image
-                      src={p.img.replace(/^http:/, 'https:')}
-                      alt={p.name}
-                      fill
-                      style={{ objectFit: "contain" }}
-                      sizes="auto"
-                      priority={true}
-                    />
-                  </div>
-
-                  <div className={styles.info}>
-                    <button>{p.name}</button>
-                    <h4>{p.price} ₹</h4>
-                    <p>{p.desc.slice(0, 70)}...</p>
-                  </div>
-                </>
-              </Link>
+    <>
+      {loading ? <Loader /> : (
+        <div className={styles.container}>
+          <h1>{data[0] ? "PRODUCTS " : "Not Available"}</h1>
+          <div className={styles.wrapper}>
+            <div className={styles.filters}>
+              <h2>Average Customer Review</h2>
+              <p>filter 1</p>
             </div>
-          ))}
+            <div className={styles.cardContainer}>
+              {data.slice().reverse().map((p) => (
+                <div className={styles.card} key={p._id}>
+                  <Link
+                    href={`/product/${p._id}`}
+                    style={{
+                      color: "#131921",
+                      textDecoration: "none",
+                    }}
+                  >
+                    <>
+                      <div className={styles.imgContainer}>
+                        <Image
+                          src={p.img.replace(/^http:/, 'https:')}
+                          alt={p.name}
+                          fill
+                          style={{ objectFit: "contain" }}
+                          sizes="auto"
+                          priority={true}
+                        />
+                      </div>
+
+                      <div className={styles.info}>
+                        <button>{p.name}</button>
+                        <h4>{p.price} ₹</h4>
+                        <p>{p.desc.slice(0, 70)}...</p>
+                      </div>
+                    </>
+                  </Link>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
