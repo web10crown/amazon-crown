90961742350b16056ccfe49c1afd986977dd5094
web10crown <neeraj13221822@gmail.com>
2024-01-07 17:07:06 +0530

    api url correction

--- request.js
+++ request.js
@@ -1,4 +1,4 @@
 const publicRequest =
-    process.env.NODE_ENV === 'production' ? "https://apiamazon.vercel.app/" : "http://localhost:3000";
+    process.env.NODE_ENV === 'production' ? "https://apiamazon.vercel.app" : "http://localhost:3000";
 
 export { publicRequest };
