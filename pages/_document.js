a0749804e8feb5e0d045a9a8eb4cee7218e26226
web10crown <neeraj13221822@gmail.com>
2024-02-04 16:43:24 +0530

    removing scrollbar from category

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head >
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin ></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" ></link>
         <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;500&display=swap" rel="stylesheet" />
       </Head>
       <body>
